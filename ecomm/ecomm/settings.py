332e3d12d50071a173e57c8658e5246f9bab1209
Priyabrata Shee <121084828+Datarush26@users.noreply.github.com>
2024-04-17 12:22:19 +0530

    Update settings.py

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -135,9 +135,4 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rekhashee84@gmail.com'
-EMAIL_HOST_PASSWORD = 'zjmrnobmlktacham'
+
